7d6e2b805e9661416e57616306c5b091d3f4bec0
Antonio Brasilio <antonioalves05555@gmail.com>
2024-03-14 03:39:35 -0300

    feat: Basic structure for svelte

---
+++ project1/src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+	let name = 'world';
+	//a src where the image will be loaded from a free image hosting
+	let src = 'https://picsum.photos/200/200';
+	let text = 'Text to <strong> enhance </strong> the <em> text </em> of the <u> page </u>!';
+</script>
+
+<img {src} alt="One imagem from {name} app" />
+<h1>Hello {name}!</h1>
+<p>{@html text}</p>
+
+<style>
+	h1 {
+		color: red;
+	}
+</style>
