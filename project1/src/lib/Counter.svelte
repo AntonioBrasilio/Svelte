f565e51c1d6613d3439c0c9775f887d6c6480132
Antonio Brasilio <antonioalves05555@gmail.com>
2024-03-15 09:38:41 -0300

    feat: Using reactive statements

--- project1/src/lib/Counter.svelte
+++ project1/src/lib/Counter.svelte
@@ -1,12 +1,19 @@
 <script>
 	let counter = 0;
 
+	$: textCounter = `The button has been clicked ${counter} times.`;
+
+	$: if (counter > 5) {
+		console.log('counter:', counter);
+	}
+
 	const increment = () => {
 		counter += 1;
 	};
 </script>
 
 <button on:click={increment}> Clicks {counter}</button>
+<h3>{textCounter}</h3>
 
 <style>
 	button {
