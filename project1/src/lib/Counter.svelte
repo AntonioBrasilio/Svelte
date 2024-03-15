ee07427b117f9bd8c3accc233a82cb35cef16510
Antonio Brasilio <antonioalves05555@gmail.com>
2024-03-15 09:58:37 -0300

    feat: Using react statements

--- project1/src/lib/Counter.svelte
+++ project1/src/lib/Counter.svelte
@@ -1,19 +1,27 @@
 <script>
-	let counter = 0;
+	let counter1 = 0;
+	let counter2 = 0;
+	let counter3 = 0;
 
-	$: textCounter = `The button has been clicked ${counter} times.`;
+	const setCounter3 = (x, y) => {
+		counter3 = x + y;
+	};
 
-	$: if (counter > 5) {
-		console.log('counter:', counter);
-	}
+	$: setCounter3(counter1, counter2); // Working only because is above the textCounter3
+	$: textCounter3 = `The value for counter 3 is ${counter3}`; // Counter3 is only updating when counter1 or counter2 changes and the setCounter is above this line
 
-	const increment = () => {
-		counter += 1;
+	const increment1 = () => {
+		counter1 += 1;
+	};
+
+	const increment2 = () => {
+		counter2 += 1;
 	};
 </script>
 
-<button on:click={increment}> Clicks {counter}</button>
-<h3>{textCounter}</h3>
+<button on:click={increment1}> Clicks {counter1}</button>
+<button on:click={increment2}> Clicks {counter2}</button>
+<h4>{textCounter3}</h4>
 
 <style>
 	button {
