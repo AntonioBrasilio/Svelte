65497ac0496f513d2fadda715546cb31ece92772
Antonio Brasilio <antonioalves05555@gmail.com>
2024-03-22 06:30:12 -0300

    feat: Creating global stores

--- project1/src/lib/pages/Home.svelte
+++ project1/src/lib/pages/Home.svelte
@@ -1,1 +1,14 @@
+<script>
+	import { onDestroy } from 'svelte';
+	import { settings } from '../stores/settings.js';
+
+	let colorScheme;
+
+	const unsubscribe = settings.subscribe((settings) => {
+		colorScheme = settings.theme;
+	});
+
+	onDestroy(unsubscribe);
+</script>
+
 <h2>Home</h2>
