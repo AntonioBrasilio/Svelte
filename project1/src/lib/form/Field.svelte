0a353a853c14499be575fb20bd2622ca88e924b1
Antonio Brasilio <antonioalves05555@gmail.com>
2024-03-23 07:08:46 -0300

    feat: Creating new Form and Field components

---
+++ project1/src/lib/form/Field.svelte
@@ -0,0 +1,32 @@
+<script>
+	import { v4 as uuid } from 'uuid';
+
+	const id = uuid();
+
+	export let name = '';
+	export let label = '';
+	export let validate = undefined;
+	export let type = undefined;
+</script>
+
+<div class="field">
+	<label for={id}>{label}</label>
+	<input {id} {name} {type} />
+</div>
+
+<style>
+	div.field {
+		margin-bottom: 10px;
+	}
+
+	label {
+		display: block;
+		margin-bottom: 5px;
+		font-size: 14px;
+	}
+	input {
+		height: 30px;
+		width: 250px;
+		max-width: 100%;
+	}
+</style>
--- project1/src/lib/pages/Home.svelte
+++ project1/src/lib/pages/Home.svelte
@@ -1,6 +1,8 @@
 <script>
 	import { location } from '../stores/location.js';
 	import { settingsStore } from '../stores/settings.js';
+	import { Field, Form } from '../form';
+	import Button from '../Button.svelte';
 </script>
 
 <h2>Home</h2>
@@ -11,3 +13,10 @@
 {:else if Object($location).error}
 	<p>{Object($location).error.message}</p>
 {/if}
+
+<Form>
+	<Field label="Username" name="username" type="text" validate={() => {}} />
+	<Field label="Email" name="email" type="text" validate={() => {}} />
+	<Field label="Password" name="password" type="text" validate={() => {}} />
+	<Button type="submit" bgColor="red">Submit</Button>
+</Form>
